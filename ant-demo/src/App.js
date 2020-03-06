cf2530446fc26e691b70eabaabc160ca5301ab50
ciyonghu <2283649807@qq.com>
2020-03-06 09:59:27 +0800

    add table component

--- ant-demo/src/App.js
+++ ant-demo/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component,useState } from 'react';
-import { Button,Form, Input, Select } from 'antd';
+import { Button,Form, Input, Select,Table, Tag } from 'antd';
 import './App.css';
-
 const { Option } = Select;
-
 const PriceInput = ({ value = {}, onChange }) => {
 	const [number, setNumber] = useState(0);
 	const [currency, setCurrency] = useState('rmb');
@@ -120,12 +118,90 @@ const Demo = () => {
 };
 
 
+
+const columns = [
+	{
+		title: 'Name',
+		dataIndex: 'name',
+		key: 'name',
+		render: text => <a>{text}</a>,
+	},
+	{
+		title: 'Age',
+		dataIndex: 'age',
+		key: 'age',
+	},
+	{
+		title: 'Address',
+		dataIndex: 'address',
+		key: 'address',
+	},
+	{
+		title: 'Tags',
+		key: 'tags',
+		dataIndex: 'tags',
+		render: tags => (
+			<span>
+        {tags.map(tag => {
+			let color = tag.length > 5 ? 'geekblue' : 'green';
+			if (tag === 'loser') {
+				color = 'volcano';
+			}
+			return (
+				<Tag color={color} key={tag}>
+					{tag.toUpperCase()}
+				</Tag>
+			);
+		})}
+      </span>
+		),
+	},
+	{
+		title: 'Action',
+		key: 'action',
+		render: (text, record) => (
+			<span>
+        <a style={{ marginRight: 16 }}>Invite {record.name}</a>
+        <a>Delete</a>
+      </span>
+		),
+	},
+];
+
+const data = [
+	{
+		key: '1',
+		name: 'John Brown',
+		age: 32,
+		address: 'New York No. 1 Lake Park',
+		tags: ['nice', 'developer'],
+	},
+	{
+		key: '2',
+		name: 'Jim Green',
+		age: 42,
+		address: 'London No. 1 Lake Park',
+		tags: ['loser'],
+	},
+	{
+		key: '3',
+		name: 'Joe Black',
+		age: 32,
+		address: 'Sidney No. 1 Lake Park',
+		tags: ['cool', 'teacher'],
+	},
+];
+
+
+
+
 class App extends Component {
 render() {
   return (
 	<div className="App">
 	  <Button type="primary">Button</Button>
 		<Demo/>
+		<Table columns={columns} dataSource={data} />
 	</div>
   );
 }
